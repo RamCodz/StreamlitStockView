fa4bc330d2fcdf0241898d277116e30bd6131d37
Desingu <desingu.p@gmail.com>
2024-07-22 21:24:43 +0530

    Add files via upload

---
+++ Utils/debug.py
@@ -0,0 +1,8 @@
+import os 
+from datetime import datetime
+import globals
+
+def debug(msg):
+    dbg_file = os.path.join(globals.dbg_filepath, str(globals.dbg_filename)) 
+    with open(dbg_file, 'a') as file:
+        file.write(str(datetime.now().strftime(globals.dt_time_format))+"--"+ str(msg) + '\n')
---
+++ Utils/globals.py
@@ -0,0 +1,11 @@
+dbg_filepath = 'G:/python/Stock_prediction/App_core/Logs/'
+dbg_filename = 'Log_stockMe_*.txt'
+equity_list_path = 'G:/python/Stock_prediction/App_core/'
+equity_list_filename = 'Equity.csv'
+data_filepath = 'G:/python/Stock_prediction/App_core/Data/'
+data_filename = 'StockMe_*.xlsx'
+
+noy = 5 ## no of years to get data from Yahoo fin
+dt_format = '%Y-%m-%d'
+dt_time_format = '%Y/%m/%d %H:%M:%S'
+today = None  ##today in '%Y-%m-%d' format
