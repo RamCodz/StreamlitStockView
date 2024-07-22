598a28bd3106b3f6ab5776fb727305b1c77647e0
Desingu <desingu.p@gmail.com>
2024-07-22 21:32:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from datetime import datetime, timedelta
-##from predict_stk import filter_stocks  --
-from debug import debug
-import globals
-from fetch_all_ticker_data import get_all_data
-from early_cherries.early_cherries import find_cherries
-from fallen_gems.fallen_gems import find_gems
-from gen_file import create_file
+from Utils.debug import debug
+import Utils.globals
+from Utils.fetch_all_ticker_data import get_all_data
+from EarlyCherries.early_cherries import find_cherries
+##from fallen_gems.fallen_gems import find_gems
+from Utils.gen_file import create_file
 
 def dbg(msg):
     debug("main-->"+str(msg))
