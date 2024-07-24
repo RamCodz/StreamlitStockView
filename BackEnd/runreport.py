a4badddcf8e3afd64d60420a9ba072e66d950df4
Ram S <ram.somu1004@gmail.com>
2024-07-24 16:26:03 +0530

    new front end

--- BackEnd/runreport.py
+++ BackEnd/runreport.py
@@ -1,15 +1,2 @@
-import pandas as pd
-from datetime import datetime
-from Utils.debug import debug
-from Utils import globals
-from Utils.fetch_all_ticker_data import get_all_data
-from EarlyCherries.early_cherries import find_cherries
-from Utils.gen_file import create_file
-
-
-def dbg(msg):
-    debug("main-->" + str(msg))
-
-
 def main():
     pass
