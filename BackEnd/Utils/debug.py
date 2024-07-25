23eebc649fbfac5e986c8cfec6168c9de93535f5
Desingu <desingu.p@gmail.com>
2024-07-25 22:36:45 +0530

    Update debug.py

--- BackEnd/Utils/debug.py
+++ BackEnd/Utils/debug.py
@@ -1,8 +1,10 @@
 import os 
 from datetime import datetime
 from BackEnd.Utils import globals
+from BackEnd.Utils.creategitfiles import create_or_update_file
 
 def debug(msg):
-    dbg_file = os.path.join(globals.dbg_filepath, str(globals.dbg_filename)) 
-    with open(dbg_file, 'a') as file:
-        file.write(str(datetime.now().strftime(globals.dt_time_format))+"--"+ str(msg) + '\n')
+    #dbg_file = os.path.join(globals.dbg_filepath, str(globals.dbg_filename)) 
+    #with open(dbg_file, 'a') as file:
+        #file.write(str(datetime.now().strftime(globals.dt_time_format))+"--"+ str(msg) + '\n')
+    create_or_update_file(str(globals.dbg_filepath) + str(globals.dbg_filename),msg)
