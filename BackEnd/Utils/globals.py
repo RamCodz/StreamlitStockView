2a3bd2d2beacd362a9bab63620175654d2da1ea2
Desingu <desingu.p@gmail.com>
2024-07-25 21:19:37 +0530

    Backend changes

---
+++ BackEnd/Utils/globals.py
@@ -0,0 +1,15 @@
+curr_dir = None
+#dbg_filepath = 'G:/python/Stock_prediction/App_core/Logs/'
+dbg_filepath = 'Logs/'
+dbg_filename = 'Log_stockMe_*.txt'
+#equity_list_path = 'G:/python/Stock_prediction/App_core/'
+equity_list_path = 'Data/Inbound/'
+equity_list_filename = 'Equity.csv'
+#data_filepath = 'G:/python/Stock_prediction/App_core/Data/'
+data_filepath = 'Data/Outbound/'
+data_filename = 'StockMe_*.xlsx'
+
+noy = 5 ## no of years to get data from Yahoo fin
+dt_format = '%Y-%m-%d'
+dt_time_format = '%Y/%m/%d %H:%M:%S'
+today = None  ##today in '%Y-%m-%d' format
--- BackEnd/runreport.py
+++ BackEnd/runreport.py
@@ -1,2 +1,62 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import os
+##from predict_stk import filter_stocks  --
+from Utils.debug import debug
+from Utils import globals
+from Utils.fetch_all_ticker_data import get_all_data
+from Scripts.early_cherries import find_cherries
+from Utils.gen_file import create_file
+
+def dbg(msg):
+    debug("main-->"+str(msg))
+
 def main():
-    pass
+    current_directory = os.getcwd()
+    globals.curr_dir = current_directory +"/"
+    globals.dbg_filepath = str(globals.curr_dir) + str(globals.dbg_filepath)
+    globals.equity_list_path = str(globals.curr_dir) + str(globals.equity_list_path)
+    globals.data_filepath = str(globals.curr_dir) + str(globals.data_filepath)
+
+    globals.today = datetime.now().strftime(globals.dt_format)
+    globals.dbg_filename = str(globals.dbg_filename).replace("*", str(globals.today))
+    globals.data_filename = globals.data_filename.replace("*",str(globals.today))
+
+    #clear dbg file if exists
+    if os.path.exists(str(globals.dbg_filepath) + str(globals.dbg_filename)):
+        with open(str(globals.dbg_filepath) + str(globals.dbg_filename), 'w') as file:
+            pass  
+
+    dbg("*********************STARTED**************************")
+    dbg("in main module")
+    dbg("Current Directory:"+str(current_directory))
+    dbg("dbg filepath" +globals.dbg_filepath)
+    dbg("dbg filename "+globals.dbg_filename)
+    dbg("excel "+str(globals.equity_list_path) + str(globals.equity_list_filename))
+
+    all_data = pd.DataFrame()
+    dbg("reading stocklist from csv")
+    StockList = pd.read_csv(str(globals.equity_list_path) + str(globals.equity_list_filename))
+
+    dbg("before fetching all tickers data ")
+    all_data = get_all_data(StockList)
+    dbg("after fetching all tickers data ")
+    all_data = all_data.rename_axis('Date').reset_index()## assigning first col name in df and make it as column
+    all_data['Date'] = pd.to_datetime(all_data['Date'])  
+
+    dbg("lets find Early cherries ")
+    cherries_ticker_dtls = find_cherries(all_data, StockList)
+    dbg("Early cherries completed ")
+
+    dbg("Lets find Fallen Gems")
+    ##gems_ticker_dtls = fallen_gems(all_data, StockList)
+    dbg("Fallen Gems Completed")
+
+
+    ##new dataframes can be added and the same needs to be handled in gen_file module
+    create_file(cherries_ticker_dtls)  
+
+    dbg("*********************COMPLETED**************************")
+
+
+main()
