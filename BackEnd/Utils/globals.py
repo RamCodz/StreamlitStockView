6e6520771947c0d40ff1464d1e7b52c9259c42df
Desingu <desingu.p@gmail.com>
2024-07-25 22:46:53 +0530

    Update globals.py

--- BackEnd/Utils/globals.py
+++ BackEnd/Utils/globals.py
@@ -3,10 +3,10 @@ curr_dir = None
 dbg_filepath = 'BackEnd/Logs/'
 dbg_filename = 'Log_stockMe_*.txt'
 #equity_list_path = 'G:/python/Stock_prediction/App_core/'
-equity_list_path = 'Data/Inbound/'
+equity_list_path = 'BackEnd/Data/Inbound/'
 equity_list_filename = 'Equity.csv'
 #data_filepath = 'G:/python/Stock_prediction/App_core/Data/'
-data_filepath = 'Data/Outbound/'
+data_filepath = 'BackEnd/Data/Outbound/'
 data_filename = 'StockMe_*.xlsx'
 
 noy = 5 ## no of years to get data from Yahoo fin
