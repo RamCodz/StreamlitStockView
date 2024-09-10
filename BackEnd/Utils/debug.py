98828e8f01021d75df5ec522117fd350507aee85
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-09-10 22:35:40 +0530

    Update debug.py

--- BackEnd/Utils/debug.py
+++ BackEnd/Utils/debug.py
@@ -8,5 +8,6 @@ def debug(msg):
     #dbg_file = os.path.join(globals.dbg_filepath, str(globals.dbg_filename)) 
     #with open(dbg_file, 'a') as file:
         #file.write(str(datetime.now().strftime(globals.dt_time_format))+"--"+ str(msg) + '\n')
-    file_path = Path(__file__).parent / 'debug_log.log'
-    create_or_update_file(str(file_path),msg)
+    #file_path = Path(__file__).parent / 'debug_log.log'
+    #create_or_update_file(str(file_path),msg)
+    pass
