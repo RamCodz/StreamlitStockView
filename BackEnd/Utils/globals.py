2bbb7e092fccfcffc4ba6e6ba437de49e5e55c23
Desingu <desingu.p@gmail.com>
2024-09-23 21:40:55 +0530

    Update globals.py

--- BackEnd/Utils/globals.py
+++ BackEnd/Utils/globals.py
@@ -13,3 +13,7 @@ noy = 1 ## no of years to get data from Yahoo fin
 dt_format = '%Y-%m-%d'
 dt_time_format = '%Y/%m/%d %H:%M:%S'
 today = None  ##today in '%Y-%m-%d' format
+
+cherry_breakout = ['5Y','1Y','6M','3M','1M']  ##as of now Year(Y) and Month(M) allowed
+gems_breakout = ['1W','1M','3M','6M','1Y']  
+gems_pct_change = 0.001
