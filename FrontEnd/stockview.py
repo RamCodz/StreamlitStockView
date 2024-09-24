da381a27bd5804197bb7c8e7d53401004e509bee
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-09-24 19:28:45 +0530

    Update stockview.py

--- FrontEnd/stockview.py
+++ FrontEnd/stockview.py
@@ -1,29 +1,37 @@
-import streamlit as st
 from BackEnd import runreport
 
 # ---Page setup
+dashboard_page = st.Page(
+    page="dashboard.py",
+    title="Dashboard",
+    icon=":material/dashboard:",
+    default=True
+)
+cherries_page = st.Page(
+    page="cherries.py",
+    title="Early Cherries",
+    icon=":material/account_circle:"
+)
+gems_page = st.Page(
+    page="gems.py",
+    title="Fallen Gems",
+    icon=":material/account_circle:"
+)
+sectors_page = st.Page(
+    page="sectors.py",
+    title="Sector Stars",
+    icon=":material/account_circle:"
+)
 st.set_page_config(layout="wide")
 
-# --Add logo to the sidebar
-st.sidebar.image("logo.png", use_column_width=True)
-
 # --Navigation setup
-pages = {
-    "Dashboard": "FrontEnd/dashboard.py",
-    "Early Cherries": "FrontEnd/cherries.py",
-    "Fallen Gems": "FrontEnd/gems.py",
-    "Sector Stars": "FrontEnd/sectors.py"
-}
-
-# --Sidebar for navigation
-selection = st.sidebar.selectbox("Select a page", list(pages.keys()))
+pg = st.navigation(pages=[dashboard_page, cherries_page, gems_page, sectors_page])
 
 # --Shared on all pages
-if st.sidebar.button("Run Report"):
-    st.sidebar.write("running report..")
-    runreport.main()
-    st.sidebar.write("Report generated")
 
-# --Run selected page
-page = pages[selection]
-exec(open(page).read())
+if st.sidebar.button("Run Report"):
+    st.write("running report..")
+    runreport.main()
+    st.write("Report generated")
+# --Run navigation
+pg.run()
