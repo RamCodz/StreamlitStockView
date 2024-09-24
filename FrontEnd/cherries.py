85d43efc8b1a2e7924ba89a0958f74ed760987c3
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-09-24 16:29:36 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -6,10 +6,8 @@ from BackEnd.Utils import globals
 from FrontEnd.Utils import get_latest_report_data
 import plotly.graph_objs as go
 
-# Read stock data from file
-latest_file_name = get_latest_report_data.get_latest_file(str(globals.data_filepath))
-st.write(latest_file_name)
-stock_list = str(globals.data_filepath) + latest_file_name
+# Read stock data from latest file
+stock_list = str(globals.data_filepath) + get_latest_report_data.get_latest_file(str(globals.data_filepath))
 
 try:
     stock_list_df = pd.read_csv(stock_list)
@@ -84,12 +82,12 @@ def display_stock_data_from_df(df, key_prefix=""):
 
 Cherry_tabs = st.tabs(["5 Year Breakout", "1 Year Breakout", "6 Month Breakout", "3 Month Breakout", "1 Month Breakout"])
 with Cherry_tabs[0]:
-    display_stock_data_from_df(stock_list_df, key_prefix="Cherries5Y")
+    display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] = '5Y')], key_prefix="Cherries5Y")
 with Cherry_tabs[1]:
-    display_stock_data_from_df(stock_list_df, key_prefix="Cherries1Y")
+    display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] = '1Y')], key_prefix="Cherries1Y")
 with Cherry_tabs[2]:
-    display_stock_data_from_df(stock_list_df, key_prefix="Cherries6M")
+    display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] = '6M')], key_prefix="Cherries6M")
 with Cherry_tabs[3]:
-    display_stock_data_from_df(stock_list_df, key_prefix="Cherries3M")
+    display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] = '3M')], key_prefix="Cherries3M")
 with Cherry_tabs[4]:
-    display_stock_data_from_df(stock_list_df, key_prefix="Cherries1M")
+    display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] = '1M')], key_prefix="Cherries1M")
