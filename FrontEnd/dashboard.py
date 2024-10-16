4370de897312a425da0d3f4285e7392c3942a36b
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 13:52:30 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -1,3 +1,28 @@
 import streamlit as st
+from pathlib import Path
+import pandas as pd
+import yfinance as yf
+from BackEnd.Utils import globals
+from FrontEnd.Utils import get_latest_report_data
+import plotly.graph_objs as go
 
-st.write("Dashboard - coming soon...")
+# Read stock data from latest file
+stock_list = str(globals.data_filepath) + get_latest_report_data.get_latest_file(str(globals.data_filepath))
+
+try:
+    stock_list_df = pd.read_csv(stock_list)
+except Exception as e:
+    st.error(f"Error reading the file: {e}")
+    stock_list_df = pd.DataFrame()
+
+cherries_5y = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '5Y')]
+cherries_1y = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '1Y')]
+cherries_6m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '6M')]
+cherries_3m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '3M')]
+cherries_1m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '1M')]
+
+common_cherries = cherries_5y
+for cherries in [cherries_1y, cherries_6m, cherries_3m, cherries_1m]:
+    common_cherries = pd.merge(common_cherries, cherries, how='inner', on=['your_common_column'])
+
+st.write(common_cherries)
