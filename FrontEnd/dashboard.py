a2ac0a81f9d9645aa620b10c829ecf1205415b8c
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 15:16:04 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -16,10 +16,15 @@ except Exception as e:
     stock_list_df = pd.DataFrame()
 
 cherries_5y = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '5Y')]
+cherries_5y = cherries_5y.drop(['Break Out', 'Variation'], axis=1)
 cherries_1y = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '1Y')]
+cherries_1y = cherries_1y.drop(['Break Out', 'Variation'], axis=1)
 cherries_6m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '6M')]
+cherries_6m = cherries_6m.drop(['Break Out', 'Variation'], axis=1)
 cherries_3m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '3M')]
+cherries_3m = cherries_3m.drop(['Break Out', 'Variation'], axis=1)
 cherries_1m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '1M')]
+cherries_1m = cherries_1m.drop(['Break Out', 'Variation'], axis=1)
 
 common_cherries = cherries_5y
 for cherries in [cherries_1y, cherries_6m, cherries_3m, cherries_1m]:
