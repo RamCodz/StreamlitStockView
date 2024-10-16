1a8345556fb1f8de89b50c7e30122e3f5359e570
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 15:43:36 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -26,10 +26,6 @@ cherries_3m = cherries_3m.drop(['Break Out', 'Variation'], axis=1)
 cherries_1m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == '1M')]
 cherries_1m = cherries_1m.drop(['Break Out', 'Variation'], axis=1)
 
-common_cherries = cherries_5y
-for cherries in [cherries_1y, cherries_6m, cherries_3m, cherries_1m]:
-    common_cherries = pd.merge(common_cherries, cherries, how='inner', on=['Security Code'])
-
 gems_5y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1W')]
 gems_1y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1M')]
 gems_6m = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '3M')]
