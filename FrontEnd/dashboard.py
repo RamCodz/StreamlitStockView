0526969bde439cd21fb14b4530b02026e1e36735
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 14:16:33 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -23,7 +23,7 @@ cherries_1m = stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['B
 
 common_cherries = cherries_5y
 for cherries in [cherries_1y, cherries_6m, cherries_3m, cherries_1m]:
-    common_cherries = pd.merge(common_cherries, cherries, how='inner')
+    common_cherries = pd.merge(common_cherries, cherries, how='inner', on=['Security Code'])
 
 gems_5y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1W')]
 gems_1y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1M')]
@@ -33,7 +33,7 @@ gems_1m = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break
 
 common_gems = gems_5y
 for gems in [gems_1y, gems_6m, gems_3m, gems_1m]:
-    common_gems = pd.merge(common_gems, gems, how='inner')
+    common_gems = pd.merge(common_gems, gems, how='inner', on=['Security Code'])
 
 st.write('Dash Board')
 if not common_cherries.empty:
