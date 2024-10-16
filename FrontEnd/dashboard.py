f65127e3ef76ea1c3fba223c77f9efbe543e72cb
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 16:13:04 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -40,6 +40,5 @@ common_gems = gems_1w[gems_1w['Security Code'].isin(common_gems['Security Code']
 if not common_cherries.empty:
     st.write('Common Cherries')
     st.write(common_cherries)
-if not common_gems.empty:
-    st.write('Common Gems')
-    st.write(gems_1y)
+st.write('Common Gems')
+st.write(gems_1y)
