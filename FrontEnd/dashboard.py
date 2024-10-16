79d7a0586352eb6b07dfe820affe6fa8dfcc1e3a
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-16 14:05:55 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -25,4 +25,20 @@ common_cherries = cherries_5y
 for cherries in [cherries_1y, cherries_6m, cherries_3m, cherries_1m]:
     common_cherries = pd.merge(common_cherries, cherries, how='inner')
 
-st.write(common_cherries)
+gems_5y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1W')]
+gems_1y = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1M')]
+gems_6m = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '3M')]
+gems_3m = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '6M')]
+gems_1m = stock_list_df[(stock_list_df['Report'] == 'G') & (stock_list_df['Break Out'] == '1Y')]
+
+common_gems = gems_5y
+for gems in [gems_1y, gems_6m, gems_3m, gems_1m]:
+    common_gems = pd.merge(common_gems, gems, how='inner')
+
+if common_cherries:
+    st.write('Common Cherries')
+    st.write(common_cherries)
+
+if common_gems:
+    st.write('Common Gems')
+    st.write(common_gems)
