4515dba67fe6e8684db94769403503c3b365a4ae
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-29 22:39:21 +0530

    Update runreport.py

--- BackEnd/runreport.py
+++ BackEnd/runreport.py
@@ -1,41 +1,47 @@
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
-##from predict_stk import filter_stocks  --
+import streamlit as st
 from BackEnd.Utils.debug import debug
 from BackEnd.Utils import globals
 from BackEnd.Utils.fetch_all_ticker_data import get_all_data
 from BackEnd.Scripts.early_cherries import find_cherries
 from BackEnd.Scripts.fallen_gems import find_gems
 from BackEnd.Utils.creategitfiles import create_or_update_file
-import streamlit as st
 
 def dbg(msg):
-    debug("main-->"+str(msg))
+    debug("process_stock_data--> " + str(msg))
 
-def main(gv_sys_date):
-    if gv_sys_date is null:
-       gv_sys_date = datetime.now()
+def process_stock_data(gv_sys_date):
+    # Convert date to the appropriate format
+    gv_sys_date = pd.to_datetime(gv_sys_date)
 
     current_directory = os.getcwd()
-    globals.curr_dir = current_directory +"/"
+    globals.curr_dir = current_directory + "/"
     globals.today = datetime.now().strftime(globals.dt_format)
-    globals.stockview_filename  = globals.stockview_filename.replace("*",str(globals.today))
-    all_data = pd.DataFrame()
-    StockList = pd.DataFrame()
+    globals.stockview_filename = globals.stockview_filename.replace("*", str(globals.today))
+    
+    # Load stock list and data
     StockList = pd.read_csv(str(globals.equity_list_path) + str(globals.equity_list_filename))
-  
     all_data = get_all_data(StockList)
-    all_data = all_data.rename_axis('Date').reset_index()## assigning first col name in df and make it as column
-    all_data['Date'] = pd.to_datetime(all_data['Date'])  
+    all_data = all_data.rename_axis('Date').reset_index()  # Assign first col name in df and make it a column
+    all_data['Date'] = pd.to_datetime(all_data['Date'])
 
+    # Filter data based on the selected date
+    all_data = all_data[all_data['Date'] == gv_sys_date]
+
+    # Find cherries and gems
     cherries_ticker_dtls = find_cherries(all_data, StockList)
     gems_ticker_dtls = find_gems(all_data, StockList)
-    final_df = pd.DataFrame()
-    final_df = pd.concat([final_df, cherries_ticker_dtls])
-    final_df = pd.concat([final_df, gems_ticker_dtls])
     
-    ##new dataframes can be added and the same needs to be handled in gen_file module
-    ##create_or_update_file(str(globals.data_filepath) + "Cherries.csv" ,cherries_ticker_dtls)
-    ##st.write("cherries path**** "+globals.data_filepath) + str(globals.cherries_filename))
-    create_or_update_file((str(globals.data_filepath) + str(globals.stockview_filename)),final_df)
+    # Concatenate results
+    final_df = pd.concat([cherries_ticker_dtls, gems_ticker_dtls])
+    
+    # Create or update the output file
+    create_or_update_file((str(globals.data_filepath) + str(globals.stockview_filename)), final_df)
+
+# Sidebar date input
+gv_sys_date = st.sidebar.date_input("Select Date", datetime.now())
+
+# Call the function to execute the logic
+process_stock_data(gv_sys_date)
