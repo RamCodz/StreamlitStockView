5604de8946d0aa0b2dab38673de6bc486f8935ca
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-30 17:20:52 +0530

    Update trigger_report.py

--- trigger_report.py
+++ trigger_report.py
@@ -2,4 +2,4 @@ from BackEnd.runreport import process_stock_data
 from datetime import datetime
 
 if __name__ == "__main__":
-    specific_function(datetime.now())
+    process_stock_data(datetime.now())
