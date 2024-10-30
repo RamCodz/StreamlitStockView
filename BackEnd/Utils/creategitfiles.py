d389879f7bb0ea9b496eca16b056e7cf3af9de88
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-10-30 21:44:42 +0530

    Update creategitfiles.py

--- BackEnd/Utils/creategitfiles.py
+++ BackEnd/Utils/creategitfiles.py
@@ -5,7 +5,7 @@ from base64 import b64encode
 from io import StringIO
 
 # Get the GitHub token from environment variables
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN") or os.getenv("FILE_TOKEN")
 
 # Function to create or update a file in a GitHub repository
 def create_or_update_file(path, content, message="Update file via Streamlit", branch="main"):
