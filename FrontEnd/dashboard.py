adfe67fdab40c1b286f4ed2147dcc1613de82829
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-12 13:24:01 +0530

    Update dashboard.py

--- FrontEnd/dashboard.py
+++ FrontEnd/dashboard.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import yfinance as yf
-from pandas.errors import EmptyDataError
 import plotly.graph_objs as go
 
 # Create sample data
@@ -45,7 +43,7 @@ def get_color(value):
         return 'background-color: white'  # White for no change
 
 # Common function to display stock data
-def display_stock_data_from_df(df, key_prefix=""):
+def display_stock_data_from_df(df):
     if not df.empty:
         for index, row in df.iterrows():
             ticker = row['Security Id']
@@ -56,20 +54,17 @@ def display_stock_data_from_df(df, key_prefix=""):
             returns = [row['1M'], row['3M'], row['6M'], row['1Y'], row['5Y']]
             colors = [get_color(value) for value in returns]
             
-            st.markdown(
-                f'<div style="padding:10px; margin:5px; border-radius:5px; display:flex; flex-direction:row; align-items:center;">' +
-                f'<div style="flex:1; {colors[0]}; padding:10px;">{tick}</div>' +
-                f'<div style="flex:1; {colors[0]}; padding:10px;">{row["1M"]}%</div>' +
-                f'<div style="flex:1; {colors[1]}; padding:10px;">{row["3M"]}%</div>' +
-                f'<div style="flex:1; {colors[2]}; padding:10px;">{row["6M"]}%</div>' +
-                f'<div style="flex:1; {colors[3]}; padding:10px;">{row["1Y"]}%</div>' +
-                f'<div style="flex:1; {colors[4]}; padding:10px;">{row["5Y"]}%</div>' +
-                '</div>', unsafe_allow_html=True
-            )
+            with st.expander(f"{tick} >>> {returns[3]}% - {sector} - {industry}"):
+                st.markdown(
+                    f'<div style="padding:10px; margin:5px; border-radius:5px; display:flex; flex-direction:row; align-items:center;">' +
+                    f'<div style="flex:1; {colors[0]}; padding:10px;">1M: {row["1M"]}%</div>' +
+                    f'<div style="flex:1; {colors[1]}; padding:10px;">3M: {row["3M"]}%</div>' +
+                    f'<div style="flex:1; {colors[2]}; padding:10px;">6M: {row["6M"]}%</div>' +
+                    f'<div style="flex:1; {colors[3]}; padding:10px;">1Y: {row["1Y"]}%</div>' +
+                    f'<div style="flex:1; {colors[4]}; padding:10px;">5Y: {row["5Y"]}%</div>' +
+                    '</div>', unsafe_allow_html=True
+                )
 
-            show_plot = st.checkbox(f"More about {tick}", key=f"{key_prefix}-{tick}")
-
-            if show_plot:
                 cherries_stock = get_stock_data(ticker)
                 if not cherries_stock.empty:
                     fig = go.Figure()
@@ -93,5 +88,16 @@ def display_stock_data_from_df(df, key_prefix=""):
     else:
         st.warning("No data available to display.")
 
-# Create and display data
-display_stock_data_from_df(stock_list_df)
+# Function to create tabs and display data
+def create_tabs(tab_titles, stock_list_df):
+    tabs = st.tabs(tab_titles)
+    for i, title in enumerate(tab_titles):
+        with tabs[i]:
+            if not stock_list_df.empty:
+                display_stock_data_from_df(stock_list_df[(stock_list_df['Report'] == 'C') & (stock_list_df['Break Out'] == title.split()[0])].sort_values(by='1Y', ascending=True))
+            else:
+                st.write("No data available to display.")
+
+# Create and display tabs
+tab_titles = ["5 Year Breakout", "1 Year Breakout", "6 Month Breakout", "3 Month Breakout", "1 Month Breakout"]
+create_tabs(tab_titles, stock_list_df)
