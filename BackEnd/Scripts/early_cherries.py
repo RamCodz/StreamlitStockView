75a21a1bd28a64e82481532a53fad74c2132e743
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-13 13:56:55 +0530

    Update early_cherries.py

--- BackEnd/Scripts/early_cherries.py
+++ BackEnd/Scripts/early_cherries.py
@@ -56,7 +56,7 @@ def find_cherries(all_data, StockList, current_date):
     
     for ticker in unique_tickers:
         print("ticker " + ticker)
-        print("current_date", ticker_data)
+        print("current_date", current_date)
         print(ticker_data)
         ticker_data = all_data[all_data['ticker'] == ticker]
         ticker_data = ticker_data[ticker_data['date'] <= current_date]
