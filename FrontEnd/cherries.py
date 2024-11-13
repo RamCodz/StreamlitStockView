67e66555826bbd2b3279ab038e68fea1671f6ca6
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-13 18:23:33 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -75,14 +75,14 @@ def display_stock_data_from_df(df, key_prefix=""):
 
             # Display the stock data with color coding for each timeframe
             st.markdown(
-                f'<div style="margin:0; padding:0; border-radius:3px; display:flex; flex-direction:row; align-items:center;" class="no-space">' +
-                f'<div style="flex:1; margin:0; padding:3px;">{tick}</div>' +  # Stock name
-                f'<div style="flex:1; {colors[0]}; margin:0; padding:3px;">{row["1W_value"]}%</div>' +
-                f'<div style="flex:1; {colors[1]}; margin:0; padding:3px;">{row["1M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[2]}; margin:0; padding:3px;">{row["3M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[3]}; margin:0; padding:3px;">{row["6M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[4]}; margin:0; padding:3px;">{row["1Y_value"]}%</div>' +
-                f'<div style="flex:1; {colors[5]}; margin:0; padding:3px;">{row["5Y_value"]}%</div>' +
+                f'<div style="margin:0; padding:0; border-radius:1px; display:flex; flex-direction:row; align-items:center;" class="no-space">' +
+                f'<div style="flex:1; margin:0; padding:1px;">{tick}</div>' +  # Stock name
+                f'<div style="flex:1; {colors[0]}; margin:0; padding:1px;">{row["1W_value"]}%</div>' +
+                f'<div style="flex:1; {colors[1]}; margin:0; padding:1px;">{row["1M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[2]}; margin:0; padding:1px;">{row["3M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[3]}; margin:0; padding:1px;">{row["6M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[4]}; margin:0; padding:1px;">{row["1Y_value"]}%</div>' +
+                f'<div style="flex:1; {colors[5]}; margin:0; padding:1px;">{row["5Y_value"]}%</div>' +
                 '</div>', unsafe_allow_html=True
             )
             
