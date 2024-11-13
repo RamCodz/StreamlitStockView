aeb69de96d164124d3db336dac3729b8295f2254
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-13 17:56:22 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -74,7 +74,7 @@ def display_stock_data_from_df(df, key_prefix=""):
             st.markdown(
                 f'<div style="margin:0; padding:0; border-radius:3px; display:flex; flex-direction:row; align-items:center;" class="no-space">' +
                 f'<div style="flex:1; margin:0; padding:1px;">' +
-                f'<button style="background: none; border: none; color: #007BFF; text-decoration: underline; cursor: pointer;" onClick="window.location.reload();">{tick}</button>' +
+                f'<button style="background: none; border: none; color: #007BFF; text-decoration: underline; cursor: pointer;" onClick="display_stock_details(row)">{tick}</button>' +
                 f'</div>' +
                 f'<div style="flex:1; {colors[0]}; margin:0; padding:3px;">{row["1W_value"]}%</div>' +
                 f'<div style="flex:1; {colors[1]}; margin:0; padding:3px;">{row["1M_value"]}%</div>' +
