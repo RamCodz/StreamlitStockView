eb63d95e73ae7c89798adf717e99f2a28aa1116c
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-13 16:49:28 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -37,13 +37,13 @@ def display_stock_data_from_df(df, key_prefix=""):
             
             st.markdown(
                 f'<div style="margin:0; padding:0; border-radius:5px; display:flex; flex-direction:row; align-items:center;" class="no-space">' +
-                f'<div style="flex:1; {colors[0]}; margin:0; padding:10px;">{tick}</div>' +
-                f'<div style="flex:1; {colors[0]}; margin:0; padding:5px;">{row["1W_value"]}%</div>' +
-                f'<div style="flex:1; {colors[0]}; margin:0; padding:5px;">{row["1M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[1]}; margin:0; padding:5px;">{row["3M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[2]}; margin:0; padding:10px;">{row["6M_value"]}%</div>' +
-                f'<div style="flex:1; {colors[3]}; margin:0; padding:10px;">{row["1Y_value"]}%</div>' +
-                f'<div style="flex:1; {colors[4]}; margin:0; padding:10px;">{row["5Y_value"]}%</div>' +
+                f'<div style="flex:1; ; margin:0; padding:3px;">{tick}</div>' +
+                f'<div style="flex:1; {colors[0]}; margin:0; padding:3px;">{row["1W_value"]}%</div>' +
+                f'<div style="flex:1; {colors[1]}; margin:0; padding:3px;">{row["1M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[2]}; margin:0; padding:3px;">{row["3M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[3]}; margin:0; padding:3px;">{row["6M_value"]}%</div>' +
+                f'<div style="flex:1; {colors[4]}; margin:0; padding:3px;">{row["1Y_value"]}%</div>' +
+                f'<div style="flex:1; {colors[5]}; margin:0; padding:3px;">{row["5Y_value"]}%</div>' +
                 '</div>', unsafe_allow_html=True
             )
     else:
