e695167ab134ce5eff790d88ec5860ea050ad54e
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-14 16:20:48 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -18,7 +18,7 @@ except Exception as e:
     st.error(f"Error reading the file: {e}")
     stock_list_df = pd.DataFrame()
 
-cherries_stock_list = stock_list_df[stock_list_df['Report'] == 'C']
+cherries_stock_list = stock_list_df[stock_list_df['Report'] == 'C'].groupby(['Industry New Name','ISubgroup Name'])
 
 tab_titles = {
     "5Y": "5 Year Breakout",
