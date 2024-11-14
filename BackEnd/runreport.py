2aacade145c6f5ae16bd705b1f9c9c77a8e1d01d
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-14 13:07:47 +0530

    Update runreport.py

--- BackEnd/runreport.py
+++ BackEnd/runreport.py
@@ -10,28 +10,23 @@ from BackEnd.Utils.creategitfiles import create_or_update_file
 
 
 def process_stock_data(gv_sys_date):
-    print("Starting process_stock_data...")
-    print("gv_sys_date : "+str(gv_sys_date))
-
     # Convert date to the appropriate format
-    gv_sys_date = gv_sys_date.strftime(globals.dt_format)
-    print("gv_sys_date : "+str(gv_sys_date))
     current_directory = os.getcwd()
     globals.curr_dir = current_directory + "/"
-    globals.today = datetime.now().strftime(globals.dt_format)
+    globals.today = datetime.strptime(gv_sys_date, globals.dt_format)
     globals.stockview_filename = globals.stockview_filename.replace("*", str(globals.today))
     
     # Load stock list and data
     StockList = pd.read_csv(str(globals.equity_list_path) + str(globals.equity_list_filename))
-    all_data = get_all_data(StockList, gv_sys_date)
+    all_data = get_all_data(StockList, globals.today)
     all_data = all_data.rename_axis('Date').reset_index()  # Assign first col name in df and make it a column
     all_data['Date'] = pd.to_datetime(all_data['Date'])
 
     # Filter data based on the selected date
-    all_data = all_data[all_data['Date'] <= gv_sys_date]
+    all_data = all_data[all_data['Date'] <= globals.today]
 
     # Find cherries and gems
-    cherries_ticker_dtls = find_cherries(all_data, StockList, gv_sys_date)
+    cherries_ticker_dtls = find_cherries(all_data, StockList, globals.today)
     # gems_ticker_dtls = find_gems(all_data, StockList)
     # Concatenate results
     # final_df = pd.concat([cherries_ticker_dtls, gems_ticker_dtls])
