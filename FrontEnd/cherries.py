f4dd5b4061192d7fdbd576ef779ee6a9d0246b68
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-21 23:32:01 +0530

    Update cherries.py

--- FrontEnd/cherries.py
+++ FrontEnd/cherries.py
@@ -8,7 +8,7 @@ from FrontEnd.Utils.display_stocks import create_tabs
 
 # Main logic to read stock data and create tabs
 stock_list_file = str(globals.data_filepath) + str(globals.current_report_name)
-st.write(stock_list_file)
+
 # Load stock data from CSV
 try:
     stock_list_df = pd.read_csv(stock_list_file)
