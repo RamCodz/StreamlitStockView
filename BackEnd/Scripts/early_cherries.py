d43282012d651944dfbaf8c413b6d97daa3466bc
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-22 16:01:49 +0530

    Update early_cherries.py

--- BackEnd/Scripts/early_cherries.py
+++ BackEnd/Scripts/early_cherries.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-def apply_flags_and_filter(calculated_ticker_dtls):    
+def find_cherries(calculated_ticker_dtls):    
     # Apply flags based on conditions and set to 'Y' or 'N' directly
     calculated_ticker_dtls['1M_FLG'] = (calculated_ticker_dtls['1W_value'] > calculated_ticker_dtls['1M_value']).map({True: 'Y', False: 'N'})
     calculated_ticker_dtls['3M_FLG'] = (calculated_ticker_dtls['1M_value'] > calculated_ticker_dtls['3M_value']).map({True: 'Y', False: 'N'})
