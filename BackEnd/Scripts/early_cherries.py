a58e27192c9bc4df94fb68c649093e09712c93dc
Ramanjaneyulu Somu <148235652+RamCodz@users.noreply.github.com>
2024-11-22 15:59:54 +0530

    Update early_cherries.py

--- BackEnd/Scripts/early_cherries.py
+++ BackEnd/Scripts/early_cherries.py
@@ -1,29 +1,18 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-def apply_flags(ticker_stklist_dtls):
-    """Apply flags if one timeframe outperforms another."""
-    ticker_stklist_dtls.loc[ticker_stklist_dtls['1W_value'] > ticker_stklist_dtls['1M_value'], '1M_FLG'] = 'Y'
-    ticker_stklist_dtls.loc[ticker_stklist_dtls['1M_value'] > ticker_stklist_dtls['3M_value'], '3M_FLG'] = 'Y'
-    ticker_stklist_dtls.loc[ticker_stklist_dtls['1M_value'] > ticker_stklist_dtls['6M_value'], '6M_FLG'] = 'Y'
-    ticker_stklist_dtls.loc[ticker_stklist_dtls['1M_value'] > ticker_stklist_dtls['1Y_value'], '1Y_FLG'] = 'Y'
-    ticker_stklist_dtls.loc[ticker_stklist_dtls['1M_value'] > ticker_stklist_dtls['5Y_value'], '5Y_FLG'] = 'Y'
+def apply_flags_and_filter(calculated_ticker_dtls):    
+    # Apply flags based on conditions and set to 'Y' or 'N' directly
+    calculated_ticker_dtls['1M_FLG'] = (calculated_ticker_dtls['1W_value'] > calculated_ticker_dtls['1M_value']).map({True: 'Y', False: 'N'})
+    calculated_ticker_dtls['3M_FLG'] = (calculated_ticker_dtls['1M_value'] > calculated_ticker_dtls['3M_value']).map({True: 'Y', False: 'N'})
+    calculated_ticker_dtls['6M_FLG'] = (calculated_ticker_dtls['1M_value'] > calculated_ticker_dtls['6M_value']).map({True: 'Y', False: 'N'})
+    calculated_ticker_dtls['1Y_FLG'] = (calculated_ticker_dtls['1M_value'] > calculated_ticker_dtls['1Y_value']).map({True: 'Y', False: 'N'})
+    calculated_ticker_dtls['5Y_FLG'] = (calculated_ticker_dtls['1M_value'] > calculated_ticker_dtls['5Y_value']).map({True: 'Y', False: 'N'})
 
-def find_cherries(calculated_ticker_dtls, all_data):
-    filtered_data_list = []
-    print(calculated_ticker_dtls)
-    for ticker in calculated_ticker_dtls['ticker'].unique():
-        ticker_data = all_data[all_data['ticker'] == ticker]
-        ticker_dtls = calculated_ticker_dtls[calculated_ticker_dtls['ticker'] == ticker]
+    # Filter data based on the 1M_value and flags
+    filtered_data = calculated_ticker_dtls[
+        (calculated_ticker_dtls['1M_value'] >= 15) & 
+        ((calculated_ticker_dtls['5Y_FLG'] == 'Y') | (calculated_ticker_dtls['1Y_FLG'] == 'Y'))
+    ]
 
-        # Apply flags based on breakout conditions
-        apply_flags(ticker_dtls)
-
-        # Filter data based on the 1M_value and flags
-        filtered_data = ticker_dtls[
-            (ticker_dtls['1M_value'] >= 15) & 
-            ((ticker_dtls['5Y_FLG'] == 'Y') | (ticker_dtls['1Y_FLG'] == 'Y'))
-        ]
-        filtered_data_list.append(filtered_data)
-
-    return pd.concat(filtered_data_list, ignore_index=True)
+    return filtered_data
